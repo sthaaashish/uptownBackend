bcd12d55d50fbf7b7577f25eb7606f6fd8029556
Aashish <as.aashish345@gmail.com>
2023-12-20 21:38:41 +0545

    changes

--- middleware/fileCheck.js
+++ middleware/fileCheck.js
@@ -1,77 +1,71 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises; // Using fs.promises for async file operations
 
 module.exports.fileCheck = (req, res, next) => {
-  console.log(req.file?.property_image);
-  console.log(req.file);
   if (req.files?.property_image) {
     const file = req.files.property_image;
     const validExts = [".jpg", ".jpeg", ".png"];
+    
     if (validExts.includes(path.extname(file.name))) {
-      file.mv(`./upload/${file.name}`, (err) => {
-        if (err) {
-        }
-        req.property_image = `/upload/${file.name}`;
+      const uploadPath = path.join(__dirname, "upload", file.name);
 
+      file.mv(uploadPath, async (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: "error",
+            message: "Internal Server Error",
+          });
+        }
+
+        req.property_image = `/upload/${file.name}`;
         return next();
       });
     } else {
       return res.status(400).json({
         status: "error",
-        message: `please provide valid image`,
+        message: "Please provide a valid image",
       });
     }
   } else {
     return res.status(400).json({
       status: "error",
-      message: `please provide image`,
+      message: "Please provide an image",
     });
   }
 };
 
-
-module.exports.updateCheck = (req, res, next) => {
-
+module.exports.updateCheck = async (req, res, next) => {
   if (req.files?.property_image && req.body?.imagePath) {
-
-
-
     const file = req.files.property_image;
     const validExts = ['.jpg', '.jpeg', '.png'];
-    if (validExts.includes(path.extname(file.name))) {
-      file.mv(`./upload/${file.name}`, (err) => {
-        if (err) {
 
+    if (validExts.includes(path.extname(file.name))) {
+      const uploadPath = path.join(__dirname, "upload", file.name);
+
+      try {
+        await file.mv(uploadPath);
+
+        if (await fs.access(req.body.imagePath, fs.constants.F_OK)) {
+          await fs.unlink(req.body.imagePath);
         }
 
-        fs.unlink(`.${req.body.imagePath}`, (err) => {
-
-        })
-
         req.property_image = `/upload/${file.name}`;
-
         return next();
-      });
-
-
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+          status: "error",
+          message: "Internal Server Error",
+        });
+      }
     } else {
       return res.status(400).json({
         status: 'error',
-        message: `please provide valid image`
+        message: 'Please provide a valid image',
       });
     }
-
-
-
-
   } else {
-    next();
+    return next();
   }
-
-
-
-
-
-
-
-}
+};
